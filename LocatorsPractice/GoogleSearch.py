ffe24094546b7520b3997e52a51d776e94933b32
abdulhannan96-ai <73661173+abdulhannan96-ai@users.noreply.github.com>
2020-11-04 17:43:42 +0500

    Update GoogleSearch.py

--- LocatorsPractice/GoogleSearch.py
+++ LocatorsPractice/GoogleSearch.py
@@ -6,18 +6,18 @@ class GoogleSearch(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        chromedriver_location = "C:/Users/Abdul Hannan/AppData/Local/Programs/Python/Python39/Drivers/chromedriver.exe"
+        chromedriver_location = "Your Driver Loaction"
         cls.driver = webdriver.Chrome(chromedriver_location)
         cls.driver.maximize_window()
 
     def test_search_automationstepbystep(self):
         self.driver.get("https://google.com")
-        self.driver.find_element_by_name("q").send_keys("Automation Step by Step")
+        self.driver.find_element_by_name("q").send_keys("Twitter")
         self.driver.find_element_by_name("btnK").click()
 
     def test_search_raghav(self):
         self.driver.get("https://google.com")
-        self.driver.find_element_by_name("q").send_keys("Raghav Pal")
+        self.driver.find_element_by_name("q").send_keys("Facebook")
         self.driver.find_element_by_name("btnK1").click()
 
     @classmethod
