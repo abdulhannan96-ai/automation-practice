11414a2fd6acf674ae65b342e8e16e840da73bc3
abdulhannan96-ai <73661173+abdulhannan96-ai@users.noreply.github.com>
2020-11-04 17:53:48 +0500

    Update Test01.py

--- UnitTests/Test01.py
+++ UnitTests/Test01.py
@@ -3,14 +3,14 @@ from selenium import webdriver
 
 class searchEngines(unittest.TestCase):
     def test_searchGoogle(self):
-        self.path = "C:/Users/Abdul Hannan/AppData/Local/Programs/Python/Python39/Drivers/chromedriver.exe"
+        self.path = "Your Driver Loaction"
         self.driver = webdriver.Chrome(self.path)
         self.driver.get("https://google.com")
         print("The title of page is:"+ self.driver.title)
         self.driver.close()
 
     def test_searchBing(self):
-        self.path = "C:/Users/Abdul Hannan/AppData/Local/Programs/Python/Python39/Drivers/chromedriver.exe"
+        self.path = "Your Driver Loaction"
         self.driver = webdriver.Chrome(self.path)
         self.driver.get("https://bing.com")
         print("The title of page is:"+ self.driver.title)
