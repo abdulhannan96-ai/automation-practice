9d2760114576965457e94ec412728d98d854dc7a
Abdul Hannan Imtiaz <73661173+abdulhannan96-ai@users.noreply.github.com>
2020-11-25 18:17:51 +0500

    Update home.py

--- PracticalTask/pages/home.py
+++ PracticalTask/pages/home.py
@@ -1,8 +1,8 @@
 class homepage():
     def __init__(self,driver):
         self.driver = driver
-        self.chapter1_linktext = "Chapter1"
-        self.chapter4_linktext = "Chapter4"
+        self.chapter1_linktext = "Chapter1" #located by linktext
+        self.chapter4_linktext = "Chapter4" #locted by linktext
 
     def click_chapter1_link(self):
         self.driver.find_element_by_link_text(self.chapter1_linktext).click()
